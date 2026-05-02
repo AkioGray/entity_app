48ecb7884af9c1fffbb57e7a2dfd694db24b60d5
AkioGray <gektarius.brawn@gmail.com>
2026-05-02 18:08:04 +0500

    Клауд котак

--- android/app/src/main/kotlin/com/example/entity_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/entity_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.entity_app
+package kz.entity.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
